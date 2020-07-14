105aaa3d91983ed8f9b5f58fb1ba98c0c2902a76
Kacper Wikieł <k.wikiel@gmail.com>
2020-07-14 02:31:22 +0200

    Passing context to the template

--- pages/views.py
+++ pages/views.py
@@ -6,5 +6,13 @@ def home_view(request,*args, **kwargs):
     return render(request, "home.html", {"a": "1"})
 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello Contact</h1>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 1337
+    }
+    return render(request, "about.html", my_context)
